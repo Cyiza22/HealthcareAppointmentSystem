b1c548b67b147ee5df3754118c338a3fce0b4a8b
Chanelle-001 <chanelleingabire01@gmail.com>
2025-04-23 11:12:47 +0200

    updates on Notification sms

--- src/Notificationservice.java
+++ src/Notificationservice.java
@@ -3,16 +3,21 @@ public interface Notificationservice {
 }
 
 class EmailNotification implements Notificationservice {
+    @Override
     public void sendNotification(String message) {
         System.out.println("Email Notification :" + message);
     }
 }
+
 class SMSNotification implements Notificationservice {
+    @Override
     public void sendNotification(String message) {
         System.out.println("SMS Notification :" + message);
     }
 }
+
 class PushNotification implements Notificationservice {
+    @Override
     public void sendNotification(String message) {
         System.out.println("Push Notification :" + message);
     }
